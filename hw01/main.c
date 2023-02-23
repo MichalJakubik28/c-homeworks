0ba43e89aade1698b99b767e37e6f499aa7f81f0
Michal Jakubík <xjakubi@fi.muni.cz>
2023-02-24 00:38:11 +0100

    Implemented input handling

--- hw01/main.c
+++ hw01/main.c
@@ -6,7 +6,45 @@
 bool encode(void)
 {
     // TODO implement
-    return false;
+    int scanned = 1;
+    while (scanned > 0) {
+      char bytes[4];
+      scanned = scanf( "%c%c%c%c", &bytes[0], &bytes[1], &bytes[2], &bytes[3]);
+      if (scanned == EOF) {
+        return true;
+      }
+      printf("scanned: %d \n", scanned);
+      for (int i = scanned; i < 4; i++) {
+        bytes[i] = 0;
+      }
+      for (int i = 0; i < 4; i++) {
+        printf("%d ", bytes[i]);
+      }
+      putchar('\n');
+
+      int info_word = 0;
+      for (int i = 0; i < 4; i++) {
+        info_word = info_word << 8;
+        info_word = info_word | bytes[i];
+      }
+      printf("Number: %d \n", info_word);
+      /*char codeword[5] = {0, 0, 0, 0, 0};
+      int curr_index = 3;
+      int curr_bit = 0;
+      char mask = 0;
+      int nearest_two_power = 4;
+      while (curr_index < 39) {
+          if (nearest_two_power == curr_index) {
+              nearest_two_power = nearest_two_power << 1;
+              curr_index += 1;
+              continue;
+          }
+          curr_index += 1;
+      }*/
+
+
+    }
+    return true;
 }
 
 bool decode(void)

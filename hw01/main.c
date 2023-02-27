535f5b8233654257b8ff27eb42617a9afbf2af04
Michal Jakubík <xjakubi@fi.muni.cz>
2023-02-27 11:54:13 +0100

    Fixed formatting

--- hw01/main.c
+++ hw01/main.c
@@ -1,14 +1,13 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
-bool encode(void) {
-
+bool encode(void)
+{
     int scanned = 1;
 
     while (1) {
-
         unsigned char byte1, byte2, byte3, byte4;
         scanned = scanf("%c%c%c%c", &byte1, &byte2, &byte3, &byte4);
 
@@ -31,7 +30,7 @@ bool encode(void) {
         unsigned long codeword = 0;
         int nearest_two_power = 4;
 
-        for (int curr_code_bit = 3; curr_code_bit < 40; curr_code_bit++) { // TODO da sa aj do 39
+        for (int curr_code_bit = 3; curr_code_bit < 40; curr_code_bit++) {
             codeword = codeword << 1;
             if (curr_code_bit == nearest_two_power) {
                 nearest_two_power = nearest_two_power << 1;
@@ -42,17 +41,16 @@ bool encode(void) {
         }
 
         unsigned int xor = 0;
-        unsigned long one = 1;
 
-        for (unsigned int power = 1; power <= 32; power = power << 1) {    // for each power
+        for (unsigned int power = 1; power <= 32; power = power << 1) { // for each power
             xor = 0;
             for (unsigned int index = power; index < 40; index++) {
                 if ((index & power) != 0) {
-                    xor = xor ^ ((codeword >> (39 - index)) & 1);
+                    xor = xor^((codeword >> (39 - index)) & 1);
                 }
             }
             if (xor == 1) {
-                codeword = codeword | (one << (39 - power));
+                codeword = codeword | (1l << (39 - power));
             }
         }
 
@@ -62,17 +60,15 @@ bool encode(void) {
             out_byte = out_byte | (codeword >> i);
             putchar(out_byte);
         }
-
     }
     return true;
 }
 
-bool decode(void) {
-    // TODO implement
+bool decode(void)
+{
     int scanned = 1;
 
     while (1) {
-
         unsigned char byte1, byte2, byte3, byte4, byte5;
         scanned = scanf("%c%c%c%c%c", &byte1, &byte2, &byte3, &byte4, &byte5);
 
@@ -101,17 +97,16 @@ bool decode(void) {
         code_word = code_word | byte5;
 
         unsigned char correction_xor = 0;
-        unsigned long one = 1;
+
         for (int i = 39; i > 0; i--) {
             if (((code_word >> i) & 1) == 1) {
                 correction_xor = correction_xor ^ (39 - i);
             }
         }
         if (correction_xor != 0) {
-            fprintf(stderr,"One-bit error in byte %d\n", correction_xor / 8);
+            fprintf(stderr, "One-bit error in byte %d\n", correction_xor / 8);
         }
-        code_word = code_word ^ (one << (39 - correction_xor));
-
+        code_word = code_word ^ (1l << (39 - correction_xor));
 
         unsigned int info_word = 0;
         int nearest_two_power = 1;
@@ -123,18 +118,6 @@ bool decode(void) {
             info_word = info_word << 1;
             info_word = info_word | ((code_word >> i) & 1);
         }
-
-        /*
-        // printing info_word
-        putchar('\n');
-        for (int i = 31; i >= 0; i--) {
-            if ((i + 1) % 8 == 0) {
-                putchar(32);
-            }
-            printf("%d", ((info_word >> i) & 1) ? 1 : 0);
-        }
-         */
-
 
         unsigned char out_byte = 0;
         for (int i = 24; i >= 0; i -= 8) {

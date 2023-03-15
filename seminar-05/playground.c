a3b6c31c72dfbff82a5592a9131b8d1f3c0e64e9
Michal Jakubík <xjakubi@fi.muni.cz>
2023-03-15 20:31:52 +0100

    Initial excercise implementation from seminar 05

--- seminar-05/playground.c
+++ seminar-05/playground.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "string_library.h"
+#include "assert.h"
+
+
+char edit_up(char c)
+{
+    return toupper(c);
+}
+
 
 void play_split(void)
 {
-    const char *original = "Ahoj svet 000 Query";
-    char result[50][256];
-    size_t size;
-    char delim = ' ';
 
-    string_split(original, result, &size, delim);
-    printf("SPLIT: \nSize: %zu\nParts: \n", size);
-    for (size_t i = 0; i < size; i++) {
-        printf("   %zu. - %s\n", i + 1, result[i]);
-    }
+    size_t length = string_length("Hello world!");
+    assert(length == 12);
+    char buffer[7];
+    string_copy(buffer, "Hello!"); // buffer will contain string "Hello!"
+    assert(strcmp(buffer, "Hello!") == 0);
+    size_t count_a = string_count_char("abba123 a", 'a');
+
+    char string[] = "Hello!";
+    string_edit(&edit_up, string);
+    assert(strcmp(string, "HELLO!") == 0);
+
+    printf("%lu\n", string_count_substr("PESPESP", "PESP"));
+    printf("HEEJ");
+//    const char *original = "Ahoj svet 000 Query";
+//    char result[50][256];
+//    size_t size;
+//    char delim = ' ';
+//
+//    string_split(original, result, &size, delim);
+//    printf("SPLIT: \nSize: %zu\nParts: \n", size);
+//    for (size_t i = 0; i < size; i++) {
+//        printf("   %zu. - %s\n", i + 1, result[i]);
+//    }
 }
 
 int main(void)
--- seminar-05/string_library.c
+++ seminar-05/string_library.c
@@ -2,6 +2,7 @@
 
 #include <signal.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "assert.h"
 
@@ -14,63 +15,113 @@
 
 size_t string_length(const char *str)
 {
-    // TODO
-    UNUSED(str);
-    NOT_IMPLEMENTED();
-    return 0;
+    if (str == NULL) {
+        return 0;
+    }
+    size_t index = 0;
+    while (1) {
+        char letter = str[index];
+        if (letter == 0) {
+            return index;
+        }
+        index += 1;
+    }
 }
 
 char *string_copy(char *result, const char *original)
 {
-    // TODO
-    UNUSED(result);
-    UNUSED(original);
-    NOT_IMPLEMENTED();
-    return NULL;
+    if (result == NULL || original == NULL) {
+        return NULL;
+    }
+    int index = 0;
+    while (original[index] != 0) {
+        result[index] = original[index];
+        index += 1;
+    }
+    result[index] = '\0';
+    return result;
 }
 
 size_t string_count_char(const char *string, char ch)
 {
-    // TODO
-    UNUSED(string);
-    UNUSED(ch);
-    NOT_IMPLEMENTED();
-    return 0;
+    if (string == NULL || ch == 0) {
+        return 0;
+    }
+    int index = 0;
+    int counter = 0;
+    while (string[index] != 0) {
+        if (string[index] == ch) {
+            counter++;
+        }
+        index += 1;
+    }
+    return counter;
 }
 
 char *string_edit(char (*fn)(char), char *string)
 {
-    // TODO
-    UNUSED(fn);
-    UNUSED(string);
-    NOT_IMPLEMENTED();
-    return NULL;
+    if (fn == NULL || string == NULL) {
+        return NULL;
+    }
+    size_t index = 0;
+    while (string[index] != '\0') {
+        string[index] = fn(string[index]);
+        index += 1;
+    }
+    return string;
 }
 
 size_t string_count_substr(const char *original, const char *substring)
 {
-    // TODO
-    UNUSED(original);
-    UNUSED(substring);
-    NOT_IMPLEMENTED();
-    return 0;
+    if (original == NULL || substring == NULL) {
+        return 0;
+    }
+    if (original[0] == '\0' || substring[0] == '\0') {
+        return 0;
+    }
+    size_t counter = 0;
+    char *last_match;
+    do {
+        last_match = strstr(original, substring);
+        original = last_match + 1;
+        counter += 1;
+    }
+    while (last_match != NULL);
+    counter--;
+
+    return counter;
 }
 
 size_t string_count_string_in_array(const char **array, const char *string)
 {
-    //TODO
-    UNUSED(array);
-    UNUSED(string);
-    NOT_IMPLEMENTED();
-    return 0;
+    if (array == NULL || string == NULL) {
+        return 0;
+    }
+    size_t count = 0;
+    size_t index = 0;
+    while (array[index] != NULL) {
+        int same = strcmp(array[index], string);
+        if (same == 0) {
+            count++;
+        }
+        index++;
+    }
+    return count;
 }
 
 void string_split(const char *original, char result[50][256], size_t *size, char delim)
 {
-    // TODO
-    UNUSED(original);
-    UNUSED(result);
-    UNUSED(size);
-    UNUSED(delim);
-    NOT_IMPLEMENTED();
+    if (original == NULL || size == NULL || result == NULL) {
+        return;
+    }
+    int counter = 0;
+    const char *start = original;
+    char *nearest_delim = strchr(original, delim);
+    while (nearest_delim != NULL) {
+        long diff = start - nearest_delim;
+        strncpy(result[counter], start, diff);
+        counter += 1;
+        start = nearest_delim + 1;
+        nearest_delim = strchr(original, delim);
+    }
 }

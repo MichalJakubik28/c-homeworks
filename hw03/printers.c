7e3992b2801ef6b3c327398f9340d5fdd7d2d116
Michal Jakubík <xjakubi@fi.muni.cz>
2023-04-12 22:57:06 +0200

    Merged printers with filters for code coverage test

--- hw03/printers.c
+++ hw03/printers.c
@@ -1,6 +1,5 @@
 #include "printers.h"
 
-#include "filters.h"
 #include "input_handling.h"
 #include "linked_list_utils.h"
 
@@ -9,6 +8,23 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <string.h>
+
+bool filter_waste_type(container_t *container1, bool filter_used, char waste_types[7])
+{
+    assert(container1 != NULL);
+    return !filter_used || strchr(waste_types, container1->waste_type) != NULL;
+}
+
+bool filter_capacity(container_t *container1, bool filter_used, const unsigned int capacity[2])
+{
+    assert(container1 != NULL);
+    return !filter_used || (container1->capacity >= capacity[0] && container1->capacity <= capacity[1]);
+}
+
+bool filter_accessibility(container_t *container1, bool filter_used, bool public)
+{
+    return !filter_used || container1->public == public;
+}
 
 void print_container(container_t *container1)
 {
